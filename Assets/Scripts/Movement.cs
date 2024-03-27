da8bf62645f498b96b0ef18ff96e1240a4eee303
wahyuwerayana <wahyuwerayana@gmail.com>
2024-03-27 12:06:39 +0700

    Fixed RigidBody2D and Adjust
    - Fixed the Movement Script
    - Adjust the Character Size

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public RigidBody2D rb; 
+    public Rigidbody2D rb; 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<RigidBody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
